c8b13d8b0ed1ebd10fd27974c7d4c7895052dac5
dhsavell <dhsavell05@gmail.com>
2020-02-09 19:15:36 -0500

    Factor out most common stuff

---
+++ Patreon.elm
@@ -0,0 +1,29 @@
+module Common.Patreon exposing (Patron, allPatrons, viewPatron)
+
+import FeatherIcons exposing (..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+type alias Patron =
+    { name : String
+    , tier : Int
+    , links : List ( FeatherIcons.Icon, String )
+    }
+
+
+allPatrons : List Patron
+allPatrons =
+    [ { name = "Thomas L"
+      , tier = 1
+      , links =
+            [ ( youtube, "https://www.youtube.com/channel/UCThULshLB0qiEoL73KxeNbQ" ) ]
+      }
+    ]
+
+
+viewPatron : Patron -> Html ()
+viewPatron patron =
+    div [ class "patron", class <| "tier-" ++ String.fromInt patron.tier ]
+        [ h5 [] (text patron.name :: List.map (\( icon, url ) -> a [ href url ] [ FeatherIcons.toHtml [ height 18 ] icon ]) patron.links)
+        ]
--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 # branchpanic.me-common
+
 Common files (styles, utils, etc.) for \*.branchpanic.me sites.
 
-This repo is meant to be used as a submodule!
+This repo is meant to be used as a submodule. Specifically, one named `Common`
+placed in `src/` along side Elm and JS source. It primarily acts as an Elm
+module, but also contains some shared styling.
+
+As you can probably tell, this has a pretty specific use case...
+
+## Dependencies
+
+Unfortuantey, being a submodule, I can't quite provide dependencies
+automatically.
+
+### Elm
+
+- `feathericons/elm-feather`
+
+## Styling
+
+This gets most of the job done:
+
+- `@import` default-colors.css
+- Change any variables to your liking
+- `@import` base.css
+- Add any further site-specific customizations
